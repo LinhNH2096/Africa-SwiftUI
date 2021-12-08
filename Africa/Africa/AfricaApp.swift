b842f352b4730d1dfcee108f160607bfe2c06cfb
Nguyễn Hồng Lĩnh <nguyenhonglinh@Nguyens-MacBook-Pro.local>
2021-12-08 16:43:37 +0700

    initial source and change app icon

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Nguyễn Hồng Lĩnh on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
