2ce5834e0dcb1276758c5871643fbc688ba7262d
Nguyễn Hồng Lĩnh <nguyenhonglinh@Nguyens-MacBook-Pro.local>
2021-12-10 08:57:15 +0700

    Create main tab view

---
+++ Africa/Africa/App/MainView/TabView/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Nguyễn Hồng Lĩnh on 09/12/2021.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map View")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Africa/Africa/App/MainView/TabView/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Nguyễn Hồng Lĩnh on 09/12/2021.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video View")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
